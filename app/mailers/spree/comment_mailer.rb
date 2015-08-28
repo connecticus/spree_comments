04bef00d8a4049de5ec8c567b019107d342ff529
Andrea Pavoni <andrea.pavoni@gmail.com>
2015-08-28 16:14:11 +0200

    small refactorings

--- app/mailers/spree/comment_mailer.rb
+++ app/mailers/spree/comment_mailer.rb
@@ -3,9 +3,12 @@ class Spree::CommentMailer < ActionMailer::Base
 
   def notify_email(comment)
     @comment = comment
-    mail to: SpreeComments::Config.emails_to_notify,
-      subject: Spree.t(:comment_email_subject, user_email: comment.user.email,
-                                               type: comment.commentable_type.constantize.class_name,
-                                               number: comment.commentable.number )
+    
+    mail to: SpreeComments::Config.emails_to_notify, subject: Spree.t(
+      :comment_email_subject,
+      user_email: comment.user.email,
+      type: comment.commentable_type.constantize.class_name,
+      number: comment.commentable.number
+    )
   end
 end
--- spec/lib/spree_comments/notification_spec.rb
+++ spec/lib/spree_comments/notification_spec.rb
@@ -11,16 +11,18 @@ describe SpreeComments::Notification do
         it 'sends an email to users defined in roles' do
           SpreeComments::Config.notification_methods = 'email'
           SpreeComments::Notification.new(create(:comment)).notify
+
           expect(Spree::CommentMailer).to have_received(:notify_email)
         end
       end
     end
 
-    context 'when notification methods does not includes' do
+    context 'when notification methods does not include' do
       context 'email' do
-        it 'does not send an email to users defined in roles' do
+        it 'does not send an email' do
           SpreeComments::Config.notification_methods = nil
           SpreeComments::Notification.new(create(:comment)).notify
+
           expect(Spree::CommentMailer).not_to have_received(:notify_email)
         end
       end
