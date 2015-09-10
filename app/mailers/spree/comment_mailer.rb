a51a687fff44285b173b8ea53ba9447783c71ee3
Andrea Pavoni <andrea.pavoni@gmail.com>
2015-09-10 11:42:00 +0200

    sets tenant-based host

--- app/mailers/spree/comment_mailer.rb
+++ app/mailers/spree/comment_mailer.rb
@@ -2,6 +2,13 @@ class Spree::CommentMailer < ActionMailer::Base
   default from: ENV['EMAIL_FROM'] || 'changeme@example.com'
 
   def notify_email(comments, date, tenant)
+    @host = if ENV.has_key?('TENANT_ENABLED')
+      hosts = ActiveSupport::JSON.decode(ENV.fetch('TENANT_HOST_HASH')).invert
+      hosts[tenant]
+    else
+      ENV['MAIL_DOMAIN']
+    end
+
     @comments = comments
     @date = date
     @tenant = tenant
